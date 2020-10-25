086458ba759841cfdddb21484637692d3cee714e
Goodbish <43109002+Goodbish@users.noreply.github.com>
2020-10-25 22:57:02 +0500

    init: star task (and finish?)

--- src/index.js
+++ src/index.js
@@ -35,12 +35,54 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    "**********":" "
 };
 
-function decode(expr) {
-    // write your solution here
-}
+function decode (nums) {
+    let result = morze(MORSE_TABLE);
+    let decoded = [];
+    let numsArr = nums.split('');
+    let subarray = []; //массив в который будет выведен результат.
+    for (let i = 0; i < Math.ceil(numsArr.length/10); i++){
+        subarray[i] = numsArr.slice((i*10), (i*10) + 10);
+        subarray[i] = subarray[i].join('');
+    }
+    for (let i = 0; i < subarray.length; i++) {
+      for (let key in result) {
+        if (subarray[i] == key) {
+          decoded[i] = result[key]
+        }
+      }
+    }
+    decoded = decoded.join('');
+    return decoded
+  }
 
 module.exports = {
-    decode
+    decode, morze
 }
+
+function morze(alphabet) {
+    let result = {};
+    for (let key in alphabet) {
+      let zeros = '0000000000';
+      zeros = zeros.split('');
+      let arr = key.split('');
+      for (let i = 0; i< key.length; i++) {
+        switch (key[i]) {
+          case '-': arr[i] = '11';
+            break;
+          case '.': arr[i] = '10';
+            break;
+        }
+      }
+      arr = arr.join('');
+      // arr = arr.join(',');
+      zeros.splice(zeros.length - arr.length, arr.length, arr);
+      if (zeros.length > 1) {
+         zeros = zeros.join('');
+      }
+      result[zeros] = alphabet[key];
+    }
+    return result
+  }
